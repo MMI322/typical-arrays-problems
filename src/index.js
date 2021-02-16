b03d58a80534d129b0a0e44bd50d0b6344756800
Maxim Maltsev <mmi3220702@gmail.com>
2021-02-16 03:01:44 +0300

    task completion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length === 0) { // checking for undefined or empty arrays
+    return 0;
+  } else {
+    return Math.min.apply(null, array); // checking for minimal element of the array
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length === 0) { // checking for undefined or empty arrays
+    return 0;
+  } else {
+    return Math.max.apply(null, array); // checking for maximum element of the array
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {  // checking for undefined or empty arrays
+    return 0;
+  } else {
+    let sum = 0;
+    array.forEach(
+      function elementSum(element) { // sum of all array elements
+        sum += element;
+      });
+    let average = sum / array.length; // the average value of the array
+    return average;
+  }
 }
